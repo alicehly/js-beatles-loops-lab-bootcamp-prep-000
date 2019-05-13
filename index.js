bfb667bc0314e0b0e4fbd7c7addd648cce7adc8f
Alice Ly Ms <alicehly@gmail.com>
2019-05-13 21:20:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function theBeatlesPlay(musicians, instruments) {
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
   "He hated the sound of his own voice"
-];
+  ];
 
 function johnLennonFacts(facts) {
 
